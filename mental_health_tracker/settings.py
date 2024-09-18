d5119bed28060e779423af00ab7d57e9efcdcde5
Ezar Surahman <akhdanezar@gmail.com>
2024-09-18 14:17:49 +0700

    add authentication for tutorial 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9w8+4#2hp9#6wm4g=d$%y1&2yf7kdn3%fjsw=$zrh*rki64g4&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","ezar-akhdan-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
