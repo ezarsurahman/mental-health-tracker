c7473d450ece298f2cd00367db685a802bfb53be
Ezar Surahman <akhdanezar@gmail.com>
2024-09-18 13:27:29 +0700

    test PWS deploy

--- main/views.py
+++ main/views.py
@@ -36,7 +36,7 @@ def show_json(request):
 def show_xml_by_id(request,id):
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
+#
 def show_json_by_id(request,id):
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json",data), content_type="application/json")
