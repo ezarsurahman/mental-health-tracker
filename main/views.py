ee1b01efb6f253f9df2b4163063bed3930f7e9fd
Ezar Surahman <akhdanezar@gmail.com>
2024-10-02 14:18:05 +0700

    tutorial 5

--- main/views.py
+++ main/views.py
@@ -3,21 +3,24 @@ from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate,login, logout
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 import datetime
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 @login_required(login_url="/login")
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
+    
     context = {
         'npm' : '2306165894',
         'name': request.user.username,
         'class': 'PBP B',
-        'mood_entries' : mood_entries,
+        
         'last_login': request.COOKIES['last_login']
     }
 
@@ -36,11 +39,11 @@ def create_mood_entry(request):
     return render(request, "create_mood_entry.html", context)
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json",data), content_type="application/json")
 
 def show_xml_by_id(request,id):
@@ -106,3 +109,16 @@ def delete_mood(request, id):
     mood.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) 
+    feelings = strip_tags(request.POST.get("feelings"))
+    mood_intensity = strip_tags(request.POST.get("mood_intensity"))
+    user = strip_tags(request.user)
+
+    new_mood = MoodEntry(mood=mood,feelings=feelings, mood_intensity=mood_intensity,user=user)
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
